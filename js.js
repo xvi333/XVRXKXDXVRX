5075424697c553e7541ea9dd5369949e617c246f
xvi333 <70185795+xvi333@users.noreply.github.com>
2020-09-03 22:20:53 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+// const opentype = require('opentype.js');
+
+// import opentype from 'opentype.js'
+
+// import { load } from 'opentype.js'
+
+// opentype.load('fonts/00BusinessHistory-Regular.woff', function(err, font) {
+//     if (err) {
+//         alert('Font could not be loaded: ' + err);
+//     } else {
+//         // Now let's display it on a canvas with id "canvas"
+//         const ctx = document.getElementById('canvas').getContext('2d');
+
+//         // Construct a Path object containing the letter shapes of the given text.
+//         // The other parameters are x, y and fontSize.
+//         // Note that y is the position of the baseline.
+//         const path = font.getPath('XVRXKXDXVRX', 0, 150, 72);
+
+//         // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
+//         path.draw(ctx);
+//     }
+// });
